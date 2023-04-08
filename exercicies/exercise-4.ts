4f74b9238707e888a1231cd7cb7931c4ce2fcd39
GabrielCastilhoV <gabrielcastilho70@gmail.com>
2023-04-08 13:24:09 -0400

    feat: create checkIfTheFirstLetterIsUppercase function

---
+++ exercicies/exercise-4.ts
@@ -0,0 +1,5 @@
+function checkIfTheFirstLetterIsUppercase(word: string): boolean {
+  return word.charAt(0) === word.charAt(0).toUpperCase();
+}
+
+module.exports = checkIfTheFirstLetterIsUppercase;
