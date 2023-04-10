f5e5dbd3d638d56a690842a3e25a879eae2245da
GabrielCastilhoV <gabrielcastilho70@gmail.com>
2023-04-10 15:52:32 -0400

    chore: add styled-components and mui

---
+++ project/src/styles/theme.ts
@@ -0,0 +1,17 @@
+const theme = {
+  font: {
+    weight: {
+      light: 400,
+      normal: 500,
+      bold: 700
+    }
+  },
+  colors: {
+    white: '#FFFFFF',
+    grey: '#F9F6FC',
+    dark: '#333333',
+    purple: '#5D00BF'
+  }
+}
+
+export { theme }
