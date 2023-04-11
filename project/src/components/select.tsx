77386c01b4153d44fbb5db666953c01b5b5b45be
GabrielCastilhoV <gabrielcastilho70@gmail.com>
2023-04-11 09:25:24 -0400

    feat: create select component

---
+++ project/src/components/select.tsx
@@ -0,0 +1,43 @@
+import { SyntheticEvent } from 'react'
+
+import { Autocomplete as MUISelect, TextField as MUIInput } from '@mui/material'
+
+type AutocompleteOption = {
+  name: string
+  value: string
+}
+
+type InputProps = {
+  label: string
+  value?: AutocompleteOption | null
+  options?: AutocompleteOption[]
+  onInputChange: (value: AutocompleteOption) => void
+}
+
+const Select = ({
+  label,
+  value = null,
+  onInputChange,
+  options = []
+}: InputProps) => {
+  const handleChange = (
+    event: SyntheticEvent,
+    newValue: AutocompleteOption | null
+  ) => {
+    onInputChange(newValue as AutocompleteOption)
+  }
+
+  return (
+    <MUISelect
+      options={options}
+      getOptionLabel={(option) => option.name}
+      value={value}
+      onChange={handleChange}
+      renderInput={(params) => (
+        <MUIInput {...params} label={label} variant="outlined" />
+      )}
+    />
+  )
+}
+
+export { Select }
